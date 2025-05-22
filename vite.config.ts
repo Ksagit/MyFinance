4e9a0c2adeea4b35749c95b0fc6a422e81d59abb
ksagit <kuba.kielaszek@gmail.com>
2025-05-23 00:09:37 +0200

    added icons

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
     react(),
     tailwindcss(),
     VitePWA({
+      includeAssets: ["favicon.svg"],
       registerType: "autoUpdate",
       injectRegister: false,
       pwaAssets: {
@@ -15,10 +16,34 @@ export default defineConfig({
         config: true,
       },
       manifest: {
-        name: "my_finance",
-        short_name: "my_finance",
+        name: "My Finance",
+        short_name: "MyFinance",
         description: "A PWA for your financial needs",
         theme_color: "#ffffff",
+        icons: [
+          {
+            src: "pwa-64x64.png",
+            sizes: "64x64",
+            type: "image/png",
+          },
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "maskable-icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
       },
       workbox: {
         globPatterns: ["**/*.{js,css,html,svg,png,ico}"],
